2ff96d142e1fdd53e60b5c0462ae5c56307a1fed
Glenn Fowler <cebjyre@cpan.org>
2009-11-11 00:59:15 +1100

    Populate keys array at runtime
    The previous version was populating the @all_test_keys array at
    compile time. I've altered this to populate the array just before
    it is used.

--- lib/CHI/Driver/Memcached/Test/Driver.pm
+++ lib/CHI/Driver/Memcached/Test/Driver.pm
@@ -22,11 +22,11 @@ foreach my $method (qw(dump_as_hash is_empty purge)) {
 # and clear by checking for all keys used during testing. Note, some keys
 # are changed in CHIDriverTests::set_standard_keys_and_values.
 #
-my @all_test_keys =
-  ( CHI::t::Driver->all_test_keys(), 'mixed', 'space', scalar( 'ab' x 100 ) );
 
 sub get_keys {
     my $self = shift;
+    my @all_test_keys =
+      ( CHI::t::Driver->all_test_keys(), 'mixed', 'space', scalar( 'ab' x 100 ) );
 
     my $values = $self->get_multi_hashref( \@all_test_keys );
     my @defined_keys = grep { defined $values->{$_} } keys(%$values);
@@ -35,6 +35,8 @@ sub get_keys {
 
 sub clear {
     my $self = shift;
+    my @all_test_keys =
+      ( CHI::t::Driver->all_test_keys(), 'mixed', 'space', scalar( 'ab' x 100 ) );
 
     foreach my $key (@all_test_keys) {
         $self->remove($key);
